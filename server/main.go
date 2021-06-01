0b247e732f5ff5ca5da9a3fc995b8f39460f90d3
walid <attar4443@gmail.com>
2021-06-01 12:35:11 +0700

    ver 1

---
+++ server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	p := os.Getenv("PORT")
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":"+p, nil)
+}
